3b0f2a71a24035e808b27512bd6331a42b95d846
mohitkumar995533-boop <mohitkumar995533@gmail.com>
2025-11-22 01:25:04 +0530

    committ3

--- ch_12_ad_python_1/.py
+++ ch_12_ad_python_1/.py
@@ -1,3 +1,5 @@
 # using walrus operator 
-if (n := len([1,2,3,4,5]))>3:
+if (n := len([1,2,3,4,5]))>6:
     print(f"list is too long({n}) element, expected <=3)")
+else:
+    print("")
--- ch_12_ad_python_1/module.py
+++ ch_12_ad_python_1/module.py
@@ -2,3 +2,4 @@ def myfunc():
     print("my name is mohit kr mahto")
     
 myfunc()
+print(__name__)
